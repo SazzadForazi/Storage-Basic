2ddfd7292d415aa07096cefb6e7724f40943a55d
Sazzad Hossain <sazzad.csebubt@gmail.com>
2021-09-28 10:24:23 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const db = {}
+
+const addtoDb = item => {
+    const db = getDb();
+    if (item in db) {
+        db[item] = db[item] + 1
+    }
+    else {
+        db[item] = 1;
+    }
+
+    console.log(db);
+}
+const removeFromDb = item => {
+    const db = getDb();
+    delete db[item];
+}
+const getDb = () => {
+    return db;
+}
